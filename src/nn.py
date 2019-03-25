f4d569b470580ebac4d5714d4d541fc348e6c9c0
Isaac Wong <iwong30@gatech.edu>
2019-03-24 22:28:04 -0400

    adding code for time and graphs and logs

--- src/nn.py
+++ src/nn.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from time import clock
 
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
@@ -10,8 +11,6 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import accuracy_score
-
-import utils
 
 
 def one_hot(num_features, train_clusters, test_clusters):
@@ -132,29 +131,32 @@ def nn(X, Y):
 	# Opening log file
 	log_path = '../logs/wine_nn.csv'
 	with open(log_path, 'w') as f:
-		f.write('train_accuracy,test_accuracy,' +
-		        'pca_train_accuracy,pca_test_accuracy,' +
-		        'ica_train_accuracy,ica_test_accuracy,' +
-		        'rca_train_accuracy,rca_test_accuracy,' +
-		        'rf_train_accuracy,rf_test_accuracy,'
-		        'km_train_accuracy,km_test_accuracy,' +
-		        'pca_km_train_accuracy,pca_km_test_accuracy,' +
-		        'ica_km_train_accuracy,ica_km_test_accuracy,' +
-		        'rca_km_train_accuracy,rca_km_test_accuracy,' +
-		        'rf_km_train_accuracy,rf_km_test_accuracy,'+
-		        'gmm_train_accuracy,gmm_test_accuracy,' +
-		        'pca_gmm_train_accuracy,pca_gmm_test_accuracy,' +
-		        'ica_gmm_train_accuracy,ica_gmm_test_accuracy,' +
-		        'rca_gmm_train_accuracy,rca_gmm_test_accuracy,' +
-		        'rf_gmm_train_accuracy,rf_gmm_test_accuracy,'+
+		f.write('time,train_accuracy,test_accuracy,' +
+		        'pca_time,pca_train_accuracy,pca_test_accuracy,' +
+		        'ica_time,ica_train_accuracy,ica_test_accuracy,' +
+		        'rca_time,rca_train_accuracy,rca_test_accuracy,' +
+		        'rf_time,rf_train_accuracy,rf_test_accuracy,'
+		        'km_time,km_train_accuracy,km_test_accuracy,' +
+		        'pca_km_time,pca_km_train_accuracy,pca_km_test_accuracy,' +
+		        'ica_km_time,ica_km_train_accuracy,ica_km_test_accuracy,' +
+		        'rca_km_time,rca_km_train_accuracy,rca_km_test_accuracy,' +
+		        'rf_km_time,rf_km_train_accuracy,rf_km_test_accuracy,'+
+		        'gmm_time,gmm_train_accuracy,gmm_test_accuracy,' +
+		        'pca_gmm_time,pca_gmm_train_accuracy,pca_gmm_test_accuracy,' +
+		        'ica_gmm_time,ica_gmm_train_accuracy,ica_gmm_test_accuracy,' +
+		        'rca_gmm_time,rca_gmm_train_accuracy,rca_gmm_test_accuracy,' +
+		        'rf_gmm_time,rf_gmm_train_accuracy,rf_gmm_test_accuracy,'+
 		        '\n')
 
 	# Training over folds
 	kf = KFold(n_splits=num_folds)
+	trans_time = np.zeros((num_folds, 5))
 	trans_train_acc = np.zeros((num_folds, 5))
 	trans_test_acc = np.zeros((num_folds, 5))
+	km_time = np.zeros((num_folds, 5))
 	km_train_acc = np.zeros((num_folds, 5))
 	km_test_acc = np.zeros((num_folds, 5))
+	gmm_time = np.zeros((num_folds, 5))
 	gmm_train_acc = np.zeros((num_folds, 5))
 	gmm_test_acc = np.zeros((num_folds, 5))
 	fold = 0
@@ -163,10 +165,23 @@ def nn(X, Y):
 		Y_train, Y_test = Y.iloc[train_index], Y.iloc[test_index]
 
 		# Dimension Reduction and transforming data to fit
+		dr_time = np.zeros(5)
+		pca_start = clock()
 		X_train_pca, X_test_pca = fit_transform_pca(pca_num_components, X_train, X_test)
+		dr_time[1] = clock() - pca_start
+
+		ica_start = clock()
 		X_train_ica, X_test_ica = fit_transform_ica(ica_num_components, X_train, X_test)
+		dr_time[2] = clock() - ica_start
+
+		rca_start = clock()
 		X_train_rca, X_test_rca = fit_transform_rca(rca_num_components, X_train, X_test)
+		dr_time[3] = clock() - rca_start
+
+		rf_start = clock()
 		X_train_rf, X_test_rf = fit_transform_rf(rf_num_components, X_train, Y_train, X_test)
+		dr_time[4] = clock() - rf_start
+		print("dim red time: {}".format(dr_time))
 
 		# Fitting and computing accuracy (after dimension reduction only)
 		transformed_train = [X_train, X_train_pca, X_train_ica, X_train_rca, X_train_rf]
@@ -174,37 +189,55 @@ def nn(X, Y):
 		for i in range(0, 5):
 			TX_train = transformed_train[i]
 			TX_test = transformed_test[i]
+			start_time = clock()
 			trans_train_acc[fold, i], trans_test_acc[fold, i] = train_predict(hidden_layer_sizes,
 			                                                                  TX_train, Y_train, TX_test, Y_test)
+			time = clock() - start_time
+			print("{} + {}".format(dr_time[i], time))
+			trans_time[fold, i] = dr_time[i] + time
+		print("dim red + training time: {}".format(trans_time[fold, :]))
 
 		# Clustering the reduced dataset and computing accuracy (dim red -> clustering)
 		k_for_kmeans = [k_kmeans, k_kmeans_pca, k_kmeans_ica, k_kmeans_rca, k_kmeans_rf]
+		k_for_gmm = [k_gmm, k_gmm_pca, k_gmm_ica, k_gmm_rca, k_gmm_rf]
 		for i in range(0, 5):
 			TX_train = transformed_train[i]
 			TX_test = transformed_test[i]
+			start_time = clock()
 			km_train_acc[fold, i], km_test_acc[fold, i] = kmeans_train_predict(k_for_kmeans[i], hidden_layer_sizes,
 			                                                                   TX_train, Y_train, TX_test, Y_test)
-			gmm_train_acc[fold, i], gmm_test_acc[fold, i] = gmm_train_predict(k_for_kmeans[i], hidden_layer_sizes,
+			time = clock() - start_time
+			print("{} + {}".format(dr_time[i], time))
+			km_time[fold, i] = dr_time[i] + time
+
+			start_time = clock()
+			gmm_train_acc[fold, i], gmm_test_acc[fold, i] = gmm_train_predict(k_for_gmm[i], hidden_layer_sizes,
 			                                                                   TX_train, Y_train, TX_test, Y_test)
+			gmm_time[fold, i] = dr_time[i] + (clock() - start_time)
+		print("dim red + km + training time: {}".format(km_time[fold, :]))
+		print("dim red + gmm + training time: {}".format(gmm_time[fold, :]))
 
 		fold += 1
 
 	# Finding mean over all trials and then output
+	trans_time = trans_time.mean(axis=0)
 	trans_train_acc = trans_train_acc.mean(axis=0)
 	trans_test_acc = trans_test_acc.mean(axis=0)
+	km_time = km_time.mean(axis=0)
 	km_train_acc = km_train_acc.mean(axis=0)
 	km_test_acc = km_test_acc.mean(axis=0)
+	gmm_time = gmm_time.mean(axis=0)
 	gmm_train_acc = gmm_train_acc.mean(axis=0)
 	gmm_test_acc = gmm_test_acc.mean(axis=0)
 
 	with open(log_path, 'a') as f:
 		output = ''
 		for i in range(0, 5):
-			output = output + ',{},{}'.format(trans_train_acc[i], trans_test_acc[i])
+			output = output + ',{},{},{}'.format(trans_time[i], trans_train_acc[i], trans_test_acc[i])
 		for i in range(0, 5):
-			output = output + ',{},{}'.format(km_train_acc[i], km_test_acc[i])
+			output = output + ',{},{},{}'.format(km_time[i], km_train_acc[i], km_test_acc[i])
 		for i in range(0, 5):
-			output = output + ',{},{}'.format(gmm_train_acc[i], gmm_test_acc[i])
+			output = output + ',{},{},{}'.format(gmm_time[i], gmm_train_acc[i], gmm_test_acc[i])
 		output = output[1:] + '\n'  # Removing the first ',' and adding a new line
 		f.write(output)
 
--- src/plot_nn.py
+++ src/plot_nn.py
@@ -4,9 +4,57 @@ import matplotlib.pyplot as plt
 
 def main():
 	log_filepath = '../logs/wine_nn.csv'
-	base_graphpath = '../graphs/wine_nn.png'
+	base_graphpath = '../graphs/wine_nn@plot@.png'
 	data = pd.read_csv(log_filepath)
 
+	## Time plot
+	time = data['time'][0]
+	pca_time = data['pca_time'][0]
+	ica_time = data['ica_time'][0]
+	rca_time = data['rca_time'][0]
+	rf_time = data['rf_time'][0]
+	pcak_time = data['pca_km_time'][0]
+	icak_time = data['ica_km_time'][0]
+	rcak_time = data['rca_km_time'][0]
+	rfk_time = data['rf_km_time'][0]
+	pcag_time = data['pca_gmm_time'][0]
+	icag_time = data['ica_gmm_time'][0]
+	rcag_time = data['rca_gmm_time'][0]
+	rfg_time = data['rf_gmm_time'][0]
+
+	# set width of bar
+	barWidth = 0.25
+
+	# set height of bar
+	ctl_bar = [time]
+	red_bar = [pca_time, ica_time, rca_time, rf_time]
+	km_bar = [pcak_time, icak_time, rcak_time, rfk_time]
+	gmm_bar = [pcag_time, icag_time, rcag_time, rfg_time]
+
+	# Set position of bar on X axis
+	r0 = [0]
+	r1 = np.arange(0, len(red_bar)) + 0.75
+	r2 = [x + barWidth for x in r1]
+	r3 = [x + barWidth for x in r2]
+
+	# Make the plot
+	plt.figure()
+	plt.title('Time taken for neural network training (including various data processing)')
+	plt.bar(r0, ctl_bar, color='#587181', width=barWidth, edgecolor='white', label='Control')
+	plt.bar(r1, red_bar, color='#7ed9b7', width=barWidth, edgecolor='white', label='Only Dim. Reduction')
+	plt.bar(r2, km_bar, color='#ff6f61', width=barWidth, edgecolor='white', label='DR + K-means')
+	plt.bar(r3, gmm_bar, color='#0392cf', width=barWidth, edgecolor='white', label='DR + GMM')
+
+	# Add xticks on the middle of the group bars
+	plt.ylabel('Time (seconds)')
+	plt.xlabel('Dimension Reduction Algorithm')
+	plt.xticks([r for r in range(0, 5)], ['Original', 'PCA', 'ICA', 'RP', 'RF'])
+
+	# Create legend & Show graphic
+	plt.legend(loc='upper right')
+	plt.savefig(base_graphpath.replace('@plot@', 'time'), dpi=300)
+
+	## Bar plot of accuracy
 	test = data['test_accuracy'][0]
 	pca_test = data['pca_test_accuracy'][0]
 	ica_test = data['ica_test_accuracy'][0]
@@ -53,7 +101,7 @@ def main():
 
 	# Create legend & Show graphic
 	plt.legend(loc='lower right')
-	plt.savefig(base_graphpath, dpi=300)
+	plt.savefig(base_graphpath.replace('@plot@', ''), dpi=300)
 	plt.show()
 
 
